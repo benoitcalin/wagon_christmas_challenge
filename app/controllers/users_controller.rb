7a6424c72acfffbd990d39e0be9fb37943eea5c6
Benoit Calin <benoit.calin@gmail.com>
2020-12-10 15:00:40 +0100

    404 user error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.batch = Batch.find(params[:user][:batch])
+    @user.batch = Batch.find(params[:user][:batch]) if params[:user][:batch] != ""
     if @user.save
       redirect_to root_path, notice: "Votre participation est bien prise en compte"
     else
